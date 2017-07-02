5b5c00ad7f0ba4f194a0d6d60c77703b65114eba
Haico27 <haico.wensink@gmail.com>
2017-07-02 17:04:25 +0200

    added seeds

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ const foodItems = [
     picture: "https://groentefruit.files.wordpress.com/2010/04/druiven4.jpg",
     tolerated: false,
     ingredients: "Pure grapes"
-  }
+  },
+  {
+    title: "Oatmeal",
+    picture: "http://www.prevention.com/sites/prevention.com/files/styles/article_main_image_2200px/public/shutterstock_318957629-oatmeal-oksana-mizina-opener.jpg?itok=i0uMfcgE",
+    tolerated: true,
+    ingredients: "Oatmeal"
+  },
+
 ]
 
 class App extends Component {
