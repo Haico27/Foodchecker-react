c06cf29a2afbb458f8acea360336506b0f7970f3
Haico27 <haico.wensink@gmail.com>
2017-07-03 15:01:45 +0200

    set up react-router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import store from './store'
+import { Router, Route, IndexRoute } from 'react-router'
+import store, { history } from './store'
 import './index.css';
 import App from './App';
+import FoodContainer from './food/FoodContainer'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App />
+    <Router history={ history }>
+      <Route path="/" component={ App }>
+        <IndexRoute component={ FoodContainer } />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
