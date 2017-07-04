82f7684930a09f529666c5bb46837660733d8b01
Haico27 <haico.wensink@gmail.com>
2017-07-05 01:03:01 +0200

    added first version of foodeditor validation

--- src/food/FoodEditor.js
+++ src/food/FoodEditor.js
@@ -16,7 +16,8 @@ class FoodEditor extends PureComponent {
       title,
       picture,
       tolerated: false,
-      ingredients
+      ingredients,
+      errors: {}
     }
   }
 
@@ -44,7 +45,7 @@ class FoodEditor extends PureComponent {
   }
 
   setTolerated(event) {
-    var checkbox = event.target
+    let checkbox = event.target
     if (checkbox.checked) {
       this.setState({
         tolerated: event.target.value === 'tolerated'
@@ -56,6 +57,26 @@ class FoodEditor extends PureComponent {
       })
     }
   }
+
+  validate(foodItem) {
+    const { title, picture, ingredients } = foodItem
+
+    let errors = {}
+
+    if (!title || title === '') errors.title = 'Please enter a title for your fooditem'
+
+    if (!picture || picture === '') errors.picture = 'Please enter an URL of a picture of your fooditem'
+
+    if (!ingredients || ingredients === '') errors.ingredients = 'Please enter the ingredients of your fooditem'
+
+    this.setState({
+      errors,
+    })
+
+    return Object.keys(errors).length === 0
+  }
+
+
 
   saveFoodItem() {
     const {
@@ -72,12 +93,17 @@ class FoodEditor extends PureComponent {
       ingredients
     }
 
-    this.props.addFoodItem(foodItem)
+    if (this.validate(foodItem)) {
+      this.props.addFoodItem(foodItem)
+      console.log(foodItem)
+    }
 
-    console.log(foodItem)
+
+
   }
 
   render() {
+    const { errors } = this.state
 
     return(
       <div className="editor">
@@ -90,6 +116,8 @@ class FoodEditor extends PureComponent {
           onChange={ this.updateTitle.bind(this) }
           onKeyDown={ this.updateTitle.bind(this) } />
 
+          { errors.title && <p className="error">{ errors.title }</p> }
+
         <input
           type="text"
           ref="picture"
@@ -99,6 +127,8 @@ class FoodEditor extends PureComponent {
           onChange={ this.updatePicture.bind(this) }
           onKeyDown={ this.updatePicture.bind(this) } />
 
+          { errors.picture && <p className="error">{ errors.picture }</p> }
+
         <input
           type="text"
           ref="ingredients"
@@ -107,6 +137,8 @@ class FoodEditor extends PureComponent {
           defaultValue={ this.state.ingredients }
           onChange={ this.updateIngredients.bind(this) }
           onKeyDown={ this.updateIngredients.bind(this) } />
+
+          { errors.ingredients && <p className="error">{ errors.ingredients }</p> }
 
         <div className="setTolerated">
           <label key='tolerated' htmlFor='tolerated'>
