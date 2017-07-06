d829001c06916c79a0707397f8bb48311142dae7
Haico27 <haico.wensink@gmail.com>
2017-07-06 15:36:55 +0200

    added content and styling to ingredientspage

--- src/food/IngredientsPage.js
+++ src/food/IngredientsPage.js
@@ -3,28 +3,38 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Title from '../components/Title'
 import fetchFooditems from '../actions/fetch'
-import { Link } from 'react-router'
 import './IngredientsPage.css'
 
 class IngredientsPage extends PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    ingredients: PropTypes.string.isRequired
+    ingredients: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
   }
 
   render() {
-    const { title, ingredients } = this.props
+    const { title, picture, ingredients } = this.props
 
     return(
       <div className="ingredients page">
-        <header>
-          <Title content={ title } />
-          <Link to={`/`} className="link">Back</Link>
+        <header >
+
+            <Title content={ title } />
+
         </header>
 
-        <main>
-          <h2>See below the ingredients of { title }:</h2>
-          <p>{ ingredients }</p>
+        <main >
+          <div className="ingredientsList" style={ {backgroundImage: `url(${picture})`} } >
+            <h2>The ingredients of { title.toLowerCase() }:</h2>
+            <p><strong>{ ingredients }</strong></p>
+            <div className="notice">
+              <p className="notification">At the moment you see on this page only a string with the ingredients you entered.
+              The goal is to present here a list with all the ingredients of the fooditem you entered, and compare
+              those ingredients to the ingredients of other items you entered. When ingredients overlap, they will get the same color.
+              So in the end you will be presented with a list of ingredients colored either green or red. In this way,
+              you get an overview of the specific ingredients that might cause your discomfort! But for now...under construction!</p>
+            </div>
+          </div>
         </main>
 
       </div>
