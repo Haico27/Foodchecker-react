deeedbddc469996080df31b169aeb7a2bec72879
Haico27 <haico.wensink@gmail.com>
2017-07-06 12:53:23 +0200

    updated aboutpage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -11,10 +11,16 @@ class AboutPage extends PureComponent {
         </header>
         <main>
           <div className="text">
-            <p>The FoodChecker is an app designed to keep track of which foods your body can, and which foods it can not handle.
-            The idea of this app came up out of personal experience. My body reacted to some foods heavily and not to others, but it
-            always was hard to know which were specifically the foods that caused problems. This app gives you an overview of those foods.
-            You have to enter the foods yourself along with the ingredients. </p>
+            <p>The FoodChecker app is designed to gain insight in which foods your body can and can not handle.
+            It is based on personal experience: you enter the fooditems and its ingredients yourself. Create your personal overview now!
+            This is how it works:</p>
+            <ol>
+              <li>You have eaten a fooditem and you feel your body reacts well on it or you feel that it causes discomfort.</li>
+              <li>You fill out the form on the homepage: name of the fooditem (title), a picture and its ingredients. </li>
+              <li>Make sure you pay attention to the checkbox 'Tolerated?': unchecked means that it causes problems, checked means it was fine to eat this particular item.</li>
+              <li>Below the formfield you get to see a list with the items you entered. Items you can tolerate are depicted in green, items not tolerated in red. </li>
+              <li>By clicking on the link 'ingredients' on each item, you get to see the ingredients you entered.</li>
+            </ol>
           </div>
         </main>
       </div>
